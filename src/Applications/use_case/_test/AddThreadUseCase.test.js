fca6f6c50df6261e5cf16b94cdb932136daff45b
latiefdole <latiefdole@gmail.com>
2023-11-29 13:17:55 +0700

    first commit

---
+++ src/Applications/use_case/_test/AddThreadUseCase.test.js
@@ -0,0 +1,46 @@
+const ThreadRepository = require('../../../Domains/threads/ThreadRepository');
+const AddThreadUseCase = require('../AddThreadUseCase');
+
+describe('AddThreadUseCase', () => {
+  it('should orchestrating the add thread action correctly', async () => {
+    /**
+     * @TODO 3
+     * Lengkapi pengujian `AddThreadUseCase` agar dapat memastikan
+     * flow/logika yang dituliskan pada `AddThreadUseCase` benar!
+     *
+     * Tentunya, di sini Anda harus melakukan Test Double
+     * untuk memalsukan implmentasi fungsi `threadRepository`.
+     */
+
+    //mocking
+    const mockThreadRepository = new ThreadRepository();
+    const mockReturnAddedThread = new AddedThread({
+      id: 'thread-123',
+      title: 'Judul Mock',
+      owner: 'user-123',
+    });
+    mockThreadRepository.addThread = jest.fn(() => Promise.resolve(mockReturnAddedThread));
+
+    const useCase = new AddThreadUseCase({
+      threadRepository: mockThreadRepository,
+    });
+    const useCasePayload = {
+      title: 'Judul Mock',
+      owner: 'user-123',
+      body: 'Isi konten',
+    };
+
+    const expectedAddedThread = new AddedThread({
+      id: 'thread-123',
+      title: 'Judul Mock',
+      owner: 'user-123',
+    });
+
+    // Executing the use case
+    const addedThread = await useCase.execute(useCasePayload);
+
+    // Assertions
+    expect(addedThread).toEqual(expectedAddedThread);
+    expect(mockThreadRepository.addThread).toHaveBeenCalledWith(useCasePayload);
+  });
+});
---
+++ src/Infrastructures/repository/_test/ThreadRepositoryPostgres.test.js
@@ -0,0 +1,93 @@
+const ThreadRepositoryPostgres = require('../ThreadRepositoryPostgres');
+const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
+const pool = require('../../database/postgres/pool');
+const ThreadsTableTestHelper = require('../../../../tests/ThreadsTableTestHelper');
+
+describe('ThreadRepositoryPostgres', () => {
+  afterEach(async () => {
+    await UsersTableTestHelper.cleanTable();
+    await ThreadsTableTestHelper.cleanTable();
+  });
+
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  describe('addThread', () => {
+    it('should persist new thread and return added thread correctly', async () => {
+      /**
+       * TODO 4
+       * Lengkapi pengujian fungsi `addThread` agar kita dapat
+       * memastikan bahwa fungsi tersebut memasukkan data ke dalam database dengan benar.
+       *
+       * Pada pengujian ini, manfaatkanlah fungsi `ThreadsTableTestHelper.findThreadById`
+       * untuk mengecek data `thread` yang ada di database berdasarkan id thread.
+       */
+      // Arrange
+      const threadRepository = new ThreadRepositoryPostgres(pool, nanoid);
+
+      // Action
+      const addedThread = await threadRepository.addThread(new NewThread(sampleThread));
+      expect(addedThread).toBeInstanceOf(AddedThread);
+      sampleThread.id = addedThread.id;
+
+      // Assert
+      const thread = await ThreadTableTestHelper.findThreadById(addedThread.id);
+      expect(thread).toHaveProperty('id');
+      expect(thread.title).toStrictEqual('Title 1');
+      expect(thread.body).toStrictEqual('Body 1');
+      expect(thread.owner).toStrictEqual(user.id);
+    });
+  });
+
+  describe('isThreadExist', () => {
+    it('should return true if thread exists', async () => {
+      // Arrange
+      await UsersTableTestHelper.addUser({ id: 'user-123' });
+      await ThreadsTableTestHelper.addThread({ id: 'thread-123', owner: 'user-123' });
+      const repository = new ThreadRepositoryPostgres(pool, {});
+
+      // Action & Assert
+      await expect(repository.isThreadExist('thread-123')).resolves.toBe(true);
+    });
+
+    it('should return false if thread not exists', async () => {
+      // Arrange
+      const repository = new ThreadRepositoryPostgres(pool, {});
+
+      // Action & Assert
+      await expect(repository.isThreadExist('thread-123')).resolves.toBe(false);
+    });
+  });
+
+  describe('getThreadById', () => {
+    it('should return null if thread not exists', async () => {
+      // Arrange
+      const repository = new ThreadRepositoryPostgres(pool, {});
+
+      // Action
+      const thread = await repository.getThreadById('thread-123');
+
+      // Assert
+      expect(thread).toBeNull();
+    });
+
+    it('should return thread correctly', async () => {
+      // Arrange
+      await UsersTableTestHelper.addUser({ id: 'user-123' });
+      await ThreadsTableTestHelper.addThread({ id: 'thread-123', owner: 'user-123' });
+      const repository = new ThreadRepositoryPostgres(pool, {});
+
+      // Action
+      const thread = await repository.getThreadById('thread-123');
+
+      // Assert
+      expect(thread.id).toEqual('thread-123');
+      expect(thread.title).toEqual('title');
+      expect(thread.body).toEqual('body');
+      expect(thread.date).toEqual(expect.any(String));
+      expect(thread.username).toEqual('dicoding');
+      expect(thread.comments).toEqual([]);
+    });
+  });
+});
