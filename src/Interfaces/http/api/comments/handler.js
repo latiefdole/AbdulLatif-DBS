e6053deace30cf453eb35c1693abac1edeb0115f
latiefdole <latiefdole@gmail.com>
2023-12-08 14:27:47 +0700

    handler

--- src/Interfaces/http/api/comments/handler.js
+++ src/Interfaces/http/api/comments/handler.js
@@ -41,8 +41,6 @@ class CommentsHandler {
      *
      * Untuk mendapatkan useCase, pastikan Anda memanfaatkan method `this._container.getInstance`
      */
-    const useCase = this._container.getInstance(DeleteCommentUseCase.name);
-    await useCase.execute({ id, owner, threadId });
 
     return {
       status: 'success',
